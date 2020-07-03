f23a988a33c212d0f5fbdfa8cb7b4faa56b6ad6b
Supak Pukdam <48548611+supakjack@users.noreply.github.com>
2020-07-03 11:04:35 +0700

    update 2
    update 2

--- application/controllers/Person.php
+++ application/controllers/Person.php
@@ -3,36 +3,31 @@
 defined('BASEPATH') or exit('No direct script access allowed');
 class Person extends CI_Controller
 {
-    public $post; // set value from post
-
 
     public function __construct()
     {
         parent::__construct();
         $this->load->model('Main_model', 'model');
-        $this->post = (object) $this->input->post();
-        $this->model->table = 'person';
-        $this->model->data = $this->post;
     }
 
     public function get()
     {
-        echo json_encode(['data' => $this->model->select()]);
+        echo json_encode(['data' => $this->model->select('person', (object) $this->input->post())]);
     }
 
     public function post()
     {
-        echo json_encode(['data' => $this->model->insert()]);
+        echo json_encode(['data' => $this->model->insert('person', (object) $this->input->post())]);
     }
 
     public function update()
     {
-        echo json_encode(['data' => $this->model->update()]);
+        echo json_encode(['data' => $this->model->update('person', (object) $this->input->post())]);
     }
 
     public function delete()
     {
-        echo json_encode(['data' => $this->model->delete()]);
+        echo json_encode(['data' => $this->model->delete('person', (object) $this->input->post())]);
     }
 }
 
--- application/models/Main_model.php
+++ application/models/Main_model.php
@@ -4,39 +4,30 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Main_model extends CI_Model
 {
-
-    public $table;
-    public $data;
-
-    public function __construct()
+    public function select($table, $data)
     {
-        parent::__construct();
-    }
-
-    public function select()
-    {
-        if (isset($this->data->id)) {
-            $this->db->where('id', $this->data->id);
+        if (isset($data->id)) {
+            $this->db->where('id', $data->id);
         }
-        return $this->db->get($this->table)->result();
+        return $this->db->get($table,)->result();
     }
 
-    public function insert()
+    public function insert($table, $data)
     {
-        return $this->db->insert($this->table, $this->data);
+        return $this->db->insert($table,  $data);
     }
 
-    public function delete()
+    public function delete($table, $data)
     {
-        $this->db->where('id', $this->data->id);
-        return $this->db->delete($this->table);
+        $this->db->where('id',  $data->id);
+        return $this->db->delete($table);
     }
 
-    public function update()
+    public function update($table, $data)
     {
-        $this->db->set($this->data);
-        $this->db->where('id', $this->data->id);
-        return $this->db->update($this->table);
+        $this->db->set($data);
+        $this->db->where('id',  $data->id);
+        return $this->db->update($table);
     }
 }
 
